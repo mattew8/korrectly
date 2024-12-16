8048aba8af7731e72d31131cb0a05e46c851a499
mattew8 <mattew4483@likelion.org>
2024-12-16 23:19:13 +0900

    📦 base settings

---
+++ README copy.md
@@ -0,0 +1,2 @@
+# TabScribe
+A Chrome extension to track, organize, and save developer documentation tabs with highlights and notes for easy reference.
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        'service-worker': resolve(__dirname, 'src/app/service-worker.ts'),
+        'side-panel': resolve(__dirname, 'src/app/side-panel.html'),
+        'content-scripts': resolve(__dirname, 'src/app/content-scripts.ts'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (
+            chunkInfo.name === 'service-worker' ||
+            chunkInfo.name === 'content-scripts'
+          ) {
+            return `src/app/[name].js`;
+          }
+          return 'assets/[name].[hash].js';
+        },
+      },
+    },
+  },
+});
