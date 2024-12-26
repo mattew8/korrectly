9501be4d7cd79a57dd937ba72b79758078b087d0
mattew8 <mattew4483@likelion.org>
2024-12-26 14:48:01 +0900

    📦 tailwindcss

--- webpack.config.ts
+++ webpack.config.ts
@@ -47,6 +47,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   plugins: [
