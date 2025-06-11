59724da36a0693c7294aa05dff813665015c2ca3
mattew8 <mattew4483@likelion.org>
2025-06-11 23:30:19 +0900

    ✨ change spell checker implement
    request to BE

--- src/shared/spell-checker/index.ts
+++ src/shared/spell-checker/index.ts
@@ -1,11 +1,8 @@
 import { SpellChecker } from './spell-checker';
-import { OpenAIClientImpl } from './lib/open-ai';
 
 export type {
   SpellCheckResult,
   SpellCheckError,
 } from './model/spell-check.model';
 
-export const spellChecker = new SpellChecker(
-  new OpenAIClientImpl(process.env.OPENAI_API_KEY || ''),
-);
+export const spellChecker = new SpellChecker();
--- src/shared/spell-checker/spell-checker.ts
+++ src/shared/spell-checker/spell-checker.ts
@@ -1,39 +1,17 @@
-import { SPELL_CHECK_PROMPT } from './data/spell-check-prompt.data';
 import { SpellCheckResult } from './model/spell-check.model';
-import { OpenAIClient } from './lib/open-ai';
 
 export class SpellChecker {
-  private openAIClient: OpenAIClient;
-
-  constructor(openAIClient: OpenAIClient) {
-    this.openAIClient = openAIClient;
-  }
-
   async checkSpell(text: string): Promise<SpellCheckResult[]> {
-    console.log('checkSpell api', text);
-    const startTime = performance.now();
-    const completion = await this.openAIClient.chatCompletionsCreate({
-      messages: [
-        {
-          role: 'system',
-          content: SPELL_CHECK_PROMPT,
-        },
-        { role: 'user', content: text },
-      ],
-      model: 'gpt-4o-mini',
+    const response = await fetch(`${process.env.SERVER_URL}/v1/spell-check`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text,
+      }),
     });
-    const endTime = performance.now();
-    console.log(`API 응답 시간: ${(endTime - startTime) / 1000}초`);
 
-    const answer = completion.choices[0]?.message?.content;
-    console.log(`API answer: ${answer}`);
-
-    if (answer === 'ERROR') {
-      throw new Error('존재하지 않는 단어입니다!');
-    }
-    if (answer === null || answer === undefined || answer.length === 0) {
-      throw new Error('검색 결과가 없습니다!');
-    }
-    return JSON.parse(answer) as SpellCheckResult[];
+    return await response.json();
   }
 }
