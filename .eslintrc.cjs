1dc8f1834a3763621d60f526ecaa4d984d17ba31
mattew8 <mattew4483@likelion.org>
2025-12-02 23:25:21 +0900

    feat: 크롬 익스텐션 context간 import 방지 lint 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,34 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          {
+            target: './src/content-script',
+            from: './src/side-panel',
+            message: 'content-script cannot import from side-panel',
+          },
+          {
+            target: './src/side-panel',
+            from: './src/content-script',
+            message: 'side-panel cannot import from content-script',
+          },
+        ],
+      },
     ],
   },
 };
